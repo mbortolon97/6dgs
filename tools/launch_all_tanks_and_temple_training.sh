8f0a1e560f0deee63c75a618ed2ab60e5a33f1e4
Matteo Bortolon <matteo.bortolon@iit.it>
2024-07-26 10:41:53 +0100

    Updated launch_all_tanks_and_temple_training.sh

--- tools/launch_all_tanks_and_temple_training.sh
+++ tools/launch_all_tanks_and_temple_training.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-python3 train.py --config /home/mbortolon/data/std_nerf_repair/inerf_TensoRF_sampling/configs/truck.txt --datadir /home/mbortolon/data/datasets/TanksAndTemple/Barn --expname tensorf_Barn_VMtt
-python3 train.py --config /home/mbortolon/data/std_nerf_repair/inerf_TensoRF_sampling/configs/truck.txt --datadir /home/mbortolon/data/datasets/TanksAndTemple/Caterpillar --expname tensorf_Caterpillar_VMtt
-python3 train.py --config /home/mbortolon/data/std_nerf_repair/inerf_TensoRF_sampling/configs/truck.txt --datadir /home/mbortolon/data/datasets/TanksAndTemple/Family --expname tensorf_Family_VMtt
-python3 train.py --config /home/mbortolon/data/std_nerf_repair/inerf_TensoRF_sampling/configs/truck.txt --datadir /home/mbortolon/data/datasets/TanksAndTemple/Ignatius --expname tensorf_Ignatius_VMtt
-python3 train.py --config /home/mbortolon/data/std_nerf_repair/inerf_TensoRF_sampling/configs/truck.txt --datadir /home/mbortolon/data/datasets/TanksAndTemple/Truck --expname tensorf_Truck_VMtt
+python3 train.py -s /data/datasets/TanksAndTemple/Barn
+python3 train.py -s /data/datasets/TanksAndTemple/Caterpillar
+python3 train.py -s /data/datasets/TanksAndTemple/Family
+python3 train.py -s /data/datasets/TanksAndTemple/Ignatius
+python3 train.py -s /data/datasets/TanksAndTemple/Truck
